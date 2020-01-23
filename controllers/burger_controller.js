a5bf4817f31d3d33afb4d96e8d023f7e073c59c7
luke-ols0 <luke.olson@outlook.com>
2020-01-23 14:26:08 -0600

    added initial index.js code to return html to the server, bug in burger_controller.js

--- controllers/burger_controller.js
+++ controllers/burger_controller.js
@@ -2,7 +2,17 @@ var express = require("express");
 
 var burger = require("../models/burger.js");
 
+var index = require("../views/index.js");
+
+
 var router = express.Router();
+
+
+router.get("/", function (req, res) {
+
+    res.render(index.render());
+
+});
 
 // exports router
 module.exports = router;
---
+++ views/index.js
@@ -0,0 +1,14 @@
+// creates function to render index.html
+function render() {
+
+    return `<div class=container>
+	<div class="row">
+		<div class="col-md-12">
+			<img src="/assets/img/burger.png" class="img-responsive center-block" id="burger-img" alt="Responsive image">
+		</div>
+    </div>`
+
+};
+
+// exports render function with index.html
+module.exports = render;
